b93ef71966e957758a1f025ba931f7be1d449946
Xeostop1 <stop1@daum.net>
2022-03-17 17:11:35 +0900

    star 문구

--- src/review01/ex_star.java
+++ src/review01/ex_star.java
@@ -4,14 +4,42 @@ public class ex_star {
 
 	public static void main(String[] args) {
 		String star="*";
-		for (int i = 5; i >=0 ; i--) {
+		for (int i = 5; i >0 ; i--) {
 			System.out.println(star);
 			star+="*";
 			//왜 안되지?
 
 		}//for
-		
-		
+
+		System.out.println("-----------------");
+
+		int num=5;
+		for (int i = 1; i < num; i++) {
+			String stars="";
+			for (int j = i; j < num; j++) {
+				stars+="*";
+			}
+			System.out.println(stars);
+
+		}
+
+		System.out.println("-------------");
+
+		int x= -10;
+		if (x>=0) {
+			System.out.println(x);
+
+		} else {
+			System.out.println(-x);
+		}
+
+
+		System.out.println("----------");
+		int y=-10;
+		int absX=y >=0?y:-y;
+		System.out.println(y);
+
+
 	}//main
 
 }//class
